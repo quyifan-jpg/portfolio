c35eb7d6b8fa6763007bd7517adbffb74aab405b
unknown <quyifan@gmail.com>
2022-05-24 14:37:01 -0400

    update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,23 @@
 export const projects = [
   {
-    title: 'Furniture ',
+    title: 'Full-stack web ',
     description: "Using  MERN Stack - from start to finish. It is a simple social media app that allows users to post the furniture for other users to send email notification, other than pay disposal fee  ",
       image: '/images/p1.png',
       tags: ['Mongo', 'Express', 'React', 'Node'],
     source: 'https://github.com/quyifan-jpg/donating-web',
     visit: 'https://obscure-oasis-86038.herokuapp.com/',
     id: 0,
+  },
+  
+  
+  {
+    title: 'Panic Mechanic',
+    description: "With group of 6 students, produced app that measures heart rate, track triggers and display stress history by utilizing MVC mode",
+    image: '/images/p4.png',
+    tags: ['Swift', 'Storyboard', 'Firebase'],
+    source: 'https://drive.google.com/drive/folders/1DMBirUqnxmVAs3VVwDNBMn08KjzpJrvN',
+    visit: 'https://github.com/Allostatech/PM_iOS',
+    id: 3,
   },
   {
     title: 'Sorting Visualizer',
@@ -18,6 +29,15 @@ export const projects = [
     id: 1,
   },
   {
+    title: 'Machine Learning',
+    description: "Implemented data cleaning, feature selection from the real world source. Trained the modified IRT (Item response theory) model with over 85% accurac",
+    image: '/images/p5.jpg',
+    tags: ['Python', 'Numpy', 'Skcit-learn'],
+    source: 'https://github.com/quyifan-jpg/UofT_machine_learning',
+    visit: 'https://github.com/quyifan-jpg/UofT_machine_learning',
+    id: 3,
+  },
+  {
     title: 'File system',
     description: "an extent-based file system that supports basic file system operations with FUSE",
       image: '/images/p3.png',
@@ -25,15 +45,6 @@ export const projects = [
     source: 'https://github.com/quyifan-jpg/Uoft_operating_systems',
     visit: 'https://github.com/quyifan-jpg/Uoft_operating_systems',
     id: 2,
-  },
-  {
-    title: 'Panic Mechanic',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/p4.png',
-    tags: ['Swift', 'Storyboard', 'Firebase'],
-    source: 'https://drive.google.com/drive/folders/1DMBirUqnxmVAs3VVwDNBMn08KjzpJrvN',
-    visit: 'https://github.com/Allostatech/PM_iOS',
-    id: 3,
   },
 ];
 
