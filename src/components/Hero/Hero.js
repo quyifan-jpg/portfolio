d88da7368751590421b6f22a9c1cab49c6334505
unknown <quyifan@gmail.com>
2022-05-24 15:23:25 -0400

    update3

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
           A Software Developer <br />
         </SectionTitle>
         <SectionText>
-        For Web/Software Developer
+        I'm a fourth-year undergrad specializing in Computer Science at the UofT who is passionate about continuously learning and contributing to innovative devices and cutting-edge technology.
         </SectionText>
         {/* <Button onClick={props.handleClick}>Learn More</Button> */}
       </LeftSection>
